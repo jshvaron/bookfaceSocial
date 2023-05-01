928687e7c814d15901ab83e9313b96ade8a38840
jshvaron <jshva@icloud.com>
2023-04-30 23:51:54 -0500

    index.js for routes completed.

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+// reassigns routes
+router.use('/user', userRoutes);
+router.use('thoughts', thoughtRoutes);
+
+module.exports = router;
